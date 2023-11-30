05ed2c2d207a11f82cde1a5a390e3343504471f4
clc <chelseygranzoescanlar@gmail.com>
2023-11-30 18:21:18 +0800

    practice on creating forms

--- lib/flutter_experiment/imm_settings_folder/imm_settings.dart
+++ lib/flutter_experiment/imm_settings_folder/imm_settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reviewer/flutter_experiment/imm_settings_folder/wallet.dart';
 
 void main() => runApp(const MyWallet());
 
@@ -43,14 +44,91 @@ class _MyFormState extends State<MyForm> {
           ),
         ],
       ),
-      body:  const Column(
+      body:   Column(
         children: [
           Center(
-            child: CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('lib/assets/satoru.jpeg'),
+            child: Column(
+              children: [
+                const CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('lib/assets/satoru.jpeg'),
+                ),
+                const Text(
+                  "Chels01",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0, height: 2.0),
+                ),
+                const Text(
+                  "+63 9672907561 · @chichi01",
+                  style: TextStyle(fontWeight: FontWeight.normal, fontSize: 15.0),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                      top: 30.0, left: 15.0, right: 15.0),
+                  child: ListTile(
+                    title: const Text('My Wallet'),
+                    tileColor: Colors.white,
+                    leading: const Icon(Icons.attach_money),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.arrow_forward_ios_outlined),
+                      onPressed: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder:(context){
+                            return const Wallet();
+                          }),
+                        );
+                      },
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 30.0, left: 15.0, right: 15.0),
+                  child: const Column(
+                    children: [
+                      ListTile(
+                        title: Text('Notification and Sounds'),
+                        tileColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10.0),
+                              topRight: Radius.circular(10.0)),
+                        ),
+                        leading: Icon(Icons.message_outlined),
+                        trailing: Icon(Icons.arrow_forward_ios_outlined),
+                      ),
+                      ListTile(
+                        title: Text('Privacy and Security'),
+                        tileColor: Colors.white,
+                        leading: Icon(Icons.lock),
+                        trailing: Icon(Icons.arrow_forward_ios_outlined),
+
+                      ),
+                      ListTile(
+                        title: Text('Data and Storage'),
+                        tileColor: Colors.white,
+                        leading: Icon(Icons.folder_copy_outlined),
+                        trailing: Icon(Icons.arrow_forward_ios_outlined),
+                      ),
+                      ListTile(
+                        title: Text('Language'),
+                        tileColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10.0),
+                              bottomRight: Radius.circular(10.0)),
+                        ),
+                        leading: Icon(Icons.language_sharp),
+                        trailing: Icon(Icons.arrow_forward_ios_outlined),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          )
+          ),
+
         ],
       )
     );
--- lib/navigate_new_new_screen/form.dart
+++ lib/navigate_new_new_screen/form.dart
@@ -9,6 +9,22 @@ class MyForm extends StatefulWidget {
 }
 
 class _MyFormState extends State<MyForm> {
+  var _productName;
+  final _productController = TextEditingController();
+
+
+  @override
+  void initState() {
+    super.initState();
+    _productController.addListener(_updateText);
+  }
+
+  void _updateText(){
+    setState(() {
+      _productName = _productController.text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,23 +32,40 @@ class _MyFormState extends State<MyForm> {
         title: const Text("Form"),
         centerTitle: true,
       ),
-      body: Center(
-        child: OutlinedButton(
-          style: OutlinedButton.styleFrom(minimumSize: const Size(200, 50)),
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context){
-                  return const Details();
-            })
-            );
-          },
-          child: Text(
-            "Submit Form".toUpperCase(),
-            style: const TextStyle(fontWeight: FontWeight.bold),
+      body: ListView(
+        children: [
+          TextFormField(
+            // onChanged: (val){
+            //   _updateText(val);
+            // },
+            controller: _productController,
+            decoration: const InputDecoration(
+              labelText: 'Product Name',
+              prefixIcon: Icon(Icons.verified_user_outlined),
+              border: OutlineInputBorder()
+            ),
           ),
-        ),
-      ),
+          Text('Product Name is ${_productController.text}')
+        ],
+      )
     );
+  }
+
+  OutlinedButton myBtn(BuildContext context) {
+    return OutlinedButton(
+        style: OutlinedButton.styleFrom(minimumSize: const Size(200, 50)),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context){
+                return const Details();
+          })
+          );
+        },
+        child: Text(
+          "Submit Form".toUpperCase(),
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      );
   }
 }
