239f5e6c0acd0746dc01f7bf2627388c4b8f02a4
chichi <chelsey-ph@bobi.homes>
2024-01-31 17:53:42 +0800

    Created first Widget type function
    Header widget function

--- lib/flutter_experiment/pet_feeder_try/petFeedingScreen.dart
+++ lib/flutter_experiment/pet_feeder_try/petFeedingScreen.dart
@@ -8,6 +8,33 @@ class PetFeedingScreen extends StatefulWidget {
 }
 
 class _PetFeedingScreenState extends State<PetFeedingScreen> {
+  //functions
+  Widget header() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          color: Colors.white,
+          icon: Icon(Icons.arrow_back_ios_new_outlined),
+        ),
+        const Text(
+          'ROJECO Pet Feeder',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const Icon(
+          Icons.edit,
+          color: Colors.white,
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -19,31 +46,11 @@ class _PetFeedingScreenState extends State<PetFeedingScreen> {
             ),
             child: Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      color: Colors.white,
-                      icon: Icon(Icons.arrow_back_ios_new_outlined),
-                    ),
-                    const Text(
-                      'ROJECO Pet Feeder',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Icon(
-                      Icons.edit,
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
+                header(),
                 ListTile(
                   leading: Icon(Icons.timelapse_sharp),
+                  title: Text('Meal Plan'),
+                  trailing: Icon(Icons.arrow),
                 )
               ],
             )),
