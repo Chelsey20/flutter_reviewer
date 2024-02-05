d43357e0c06701bae438c6092169c3046547adce
chichi <chelseygranzoescanlar@gmail.com>
2024-02-05 18:24:29 +0800

    Added Quick Button

--- lib/flutter_experiment/pet_feeder_try/petFeedingScreen.dart
+++ lib/flutter_experiment/pet_feeder_try/petFeedingScreen.dart
@@ -61,6 +61,13 @@ class _PetFeedingScreenState extends State<PetFeedingScreen> {
                 MaterialPageRoute(builder: (context) => MealPlanScreen()));
           },
         ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: Divider(
+            color: Colors.white,
+            thickness: 0.5,
+          ),
+        ),
         GestureDetector(
           child: const ListTile(
             leading: Icon(
@@ -87,9 +94,10 @@ class _PetFeedingScreenState extends State<PetFeedingScreen> {
     );
   }
 
+  var PortionNumber = false;
   Widget portion() {
     return SizedBox(
-      width: MediaQuery.sizeOf(context).width,
+      //width: MediaQuery.sizeOf(context).width,
       child: ListView.builder(
           shrinkWrap: true,
           scrollDirection: Axis.horizontal,
@@ -100,29 +108,46 @@ class _PetFeedingScreenState extends State<PetFeedingScreen> {
             return Align(
               alignment: Alignment.topCenter,
               child: Container(
+                alignment: Alignment.center,
                 width: 50,
                 height: 50,
-                padding: const EdgeInsets.all(2.0),
+                // padding: const EdgeInsets.all(2.0),
                 margin: const EdgeInsets.all(10.0),
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: Colors.grey,
-                    width: 3.0,
+                    width: 1.5,
                   ),
-                  borderRadius: BorderRadius.circular(10.0),
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
-                child: Center(
-                  child: Text(
-                    '$num  ',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.grey,
-                    ),
+                child: Text(
+                  '$num',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w800,
+                    fontSize: 20.0,
+                    color: Colors.grey,
                   ),
                 ),
               ),
             );
           }),
+    );
+  }
+
+  Widget quickButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.orange,
+        minimumSize: Size(150, 40),
+      ),
+      child: const Text(
+        'Quick',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 15.0,
+        ),
+      ),
     );
   }
 
@@ -146,7 +171,18 @@ class _PetFeedingScreenState extends State<PetFeedingScreen> {
                 ],
               ),
             ),
+            const SizedBox(
+              height: 25.0,
+            ),
             Flexible(child: portion()),
+            const Text(
+              'Portion',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20.0,
+              ),
+            ),
+            quickButton(),
           ],
         ),
       ),
